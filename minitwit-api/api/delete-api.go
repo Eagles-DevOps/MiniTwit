1fc7f22ad40825ebffd53652421d6c6c6c289bfd
Hopeberg <anam@itu.dk>
2024-02-25 21:59:30 +0100

    Implemented delete to clean up in database before tests

--- minitwit-api/api/delete-api.go
+++ minitwit-api/api/delete-api.go
@@ -8,35 +8,32 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 
 	"minitwit.com/db"
-	"minitwit.com/model"
 	"minitwit.com/sim"
 )
 
 func Delete(w http.ResponseWriter, r *http.Request) {
-	sim.UpdateLatest(r)
-	dec := json.NewDecoder(r.Body)
-	var rv model.DeleteData
-	err := dec.Decode(&rv)
-	if err != nil {
-		fmt.Println("Error in requestData")
-	}
+	sqlite_db, _ := db.Connect_db()
+	defer sqlite_db.Close()
+
 	fmt.Println("yeeeeet")
 	is_auth := sim.Is_authenticated(w, r)
 	if !is_auth {
 		return
 	}
+	user_ids := make([]any, 4)
+	usernames := []string{"a", "b", "c", "test"}
 
-	if rv.User != "" && r.Method == "POST" {
-		toDeleteUsername := rv.User
-		toDeleteUser_id, _ := db.Get_user_id(toDeleteUsername)
-		query := `DELETE FROM user WHERE user_id = ?`
-
-		sqlite_db, _ := db.Connect_db()
-
-		_, err = sqlite_db.Exec(query, toDeleteUser_id)
-
-		fmt.Println(err)
-
+	for i, username := range usernames {
+		user_id, _ := db.Get_user_id(username)
+		user_ids[i] = user_id
 	}
 
+	for _, userID := range user_ids {
+		if !db.IsNil(userID) {
+			query := `DELETE FROM user WHERE user_id = ?`
+			_, err := sqlite_db.Exec(query, userID)
+			fmt.Println(userID, err)
+		}
+	}
+	json.NewEncoder(w).Encode(http.StatusOK)
 }
