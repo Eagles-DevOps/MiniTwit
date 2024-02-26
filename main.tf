2f6ad7fba8a3057c413bcbef6407b5db9b1c1095
Roman <58208649+Romes8@users.noreply.github.com>
2024-02-26 17:35:00 +0100

    Added initial version of action

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">= 1.0.0"
+ 
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+variable "do_token" {
+  description = "DigitalOcean API Token"
+  type        = string
+}
+ 
+provider "digitalocean" {
+  token = var.do_token
+}
+ 
+resource "digitalocean_droplet" "main-app-terraform-test" {
+  image  = "ubuntu-22-04-x64"
+  name   = "main-app-terraform-test"
+  region = "ams3"
+  size   = "s-1vcpu-1gb"
+}
+
+output "droplet_ip" {
+  value = digitalocean_droplet.main-app-terraform-test.ipv4_address
+  description = "The public IP address of the droplet."
+}
+
