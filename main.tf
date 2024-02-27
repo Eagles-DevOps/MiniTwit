0dd0ac1ebbae37915a904b60f886ec3a6cefbc27
Hopeberg <anam@itu.dk>
2024-02-27 19:21:13 +0100

    terraform to use docker images

--- main.tf
+++ main.tf
@@ -17,17 +17,36 @@ variable "do_token" {
 provider "digitalocean" {
   token = var.do_token
 }
+
+variable "docker_image_main" {
+  description = "Docker image for the main application"
+  type        = string
+  default     = "/minitwit_main:latest"
+}
  
 resource "digitalocean_droplet" "main-app" {
   image  = "ubuntu-22-04-x64"
   name   = "main-app"
   region = "ams3"
   size   = "s-1vcpu-1gb"
+  
+  user_data = <<-EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y docker.io
+    docker run -d -p 80:80 ${var.docker_image_main}
+  EOF
 }
 
 output "droplet_ip_main_app" {
   value       = digitalocean_droplet.main-app.ipv4_address
-  description = "The public IP address of the droplet."
+  description = "The public IP address of the main-app droplet."
+}
+
+variable "docker_image_api" {
+  description = "Docker image for the API"
+  type        = string
+  default     = "/minitwit_api:latest"
 }
 
 resource "digitalocean_droplet" "api" {
@@ -35,9 +54,16 @@ resource "digitalocean_droplet" "api" {
   name   = "api"
   region = "ams3"
   size   = "s-1vcpu-1gb"
+  
+  user_data = <<-EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y docker.io
+    docker run -d -p 80:80 ${var.docker_image_api}
+  EOF
 }
 
 output "droplet_ip_api" {
   value       = digitalocean_droplet.api.ipv4_address 
-  description = "The public IP address of the droplet."
+  description = "The public IP address of the api droplet."
 }
