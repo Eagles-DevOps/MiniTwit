1205c49564c1a808998d1c9bee153ae5d023a1c1
Roman <58208649+Romes8@users.noreply.github.com>
2024-02-27 16:28:47 +0100

    updated terrafrom file

--- main.tf
+++ main.tf
@@ -13,9 +13,11 @@ variable "do_token" {
   description = "DigitalOcean API Token"
   type        = string
 }
+
+variable "digital_ocean_token" {}
  
 provider "digitalocean" {
-  token = var.do_token
+  token = var.digital_ocean_token
 }
  
 resource "digitalocean_droplet" "main-app" {
