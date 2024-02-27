0300b0189834f2870a39bd1fd9bc45eb93e5d058
Hopeberg <anam@itu.dk>
2024-02-27 18:56:02 +0100

    config main.tf

--- main.tf
+++ main.tf
@@ -25,6 +25,30 @@ resource "digitalocean_droplet" "main-app" {
   size   = "s-1vcpu-1gb"
 }
 
+connection {
+    type     = "ssh"
+    user     = "root"
+    private_key = file("~/.ssh/id_rsa")  # Path to your SSH private key
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y golang",
+      "mkdir -p /usr/src/app",
+      "cd /usr/src/app",
+      "git clone https://github.com/Eagles-DevOps/MiniTwit.git",
+      "cd MiniTwit/minitwit-web-app",
+      "go run minitwit.go",  # Assuming your Go files are named main.go
+    ]
+  }
+}
+
+output "droplet_ip_main_app" {
+  value       = digitalocean_droplet.main-app.ipv4_address
+  description = "The public IP address of the droplet."
+}
+
 resource "digitalocean_droplet" "api" {
   image  = "ubuntu-22-04-x64"
   name   = "api"
@@ -32,9 +56,23 @@ resource "digitalocean_droplet" "api" {
   size   = "s-1vcpu-1gb"
 }
 
-output "droplet_ip_main_app" {
-  value       = digitalocean_droplet.main-app.ipv4_address
-  description = "The public IP address of the droplet."
+connection {
+    type     = "ssh"
+    user     = "root"
+    private_key = file("~/.ssh/id_rsa")  # Path to your SSH private key
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y golang",
+      "mkdir -p /usr/src/app",
+      "cd /usr/src/app",
+      "git clone https://github.com/Eagles-DevOps/MiniTwit.git",
+      "cd MiniTwit/minitwit-api",
+      "go run minitwit-api.go",  # Assuming your Go files are named main.go
+    ]
+  }
 }
 
 output "droplet_ip_api" {
