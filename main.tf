9368b70254f0e9728cdda48acdd4753537c7fba8
Hopeberg <anam@itu.dk>
2024-02-28 12:37:42 +0100

    spinning two new droplets

--- main.tf
+++ main.tf
@@ -17,25 +17,12 @@ variable "do_token" {
 provider "digitalocean" {
   token = var.do_token
 }
-
-variable "docker_image_main" {
-  description = "Docker image for the main application"
-  type        = string
-  default     = "/minitwit_main:latest"
-}
  
 resource "digitalocean_droplet" "main-app" {
   image  = "ubuntu-22-04-x64"
   name   = "main-app"
   region = "ams3"
   size   = "s-1vcpu-1gb"
-  
-  user_data = <<-EOF
-    #!/bin/bash
-    apt-get update
-    apt-get install -y docker.io
-    docker run -d -p 80:80 ${var.docker_image_main}
-  EOF
 }
 
 output "droplet_ip_main_app" {
@@ -43,24 +30,11 @@ output "droplet_ip_main_app" {
   description = "The public IP address of the main-app droplet."
 }
 
-variable "docker_image_api" {
-  description = "Docker image for the API"
-  type        = string
-  default     = "/minitwit_api:latest"
-}
-
 resource "digitalocean_droplet" "api" {
   image  = "ubuntu-22-04-x64"
   name   = "api"
   region = "ams3"
   size   = "s-1vcpu-1gb"
-  
-  user_data = <<-EOF
-    #!/bin/bash
-    apt-get update
-    apt-get install -y docker.io
-    docker run -d -p 80:80 ${var.docker_image_api}
-  EOF
 }
 
 output "droplet_ip_api" {
