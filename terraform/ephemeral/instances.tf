2ca24ed707c4de14ee449863e1ba35a28a9521ed
Jan Lishak <janlishak@users.noreply.github.com>
2024-02-29 17:55:19 +0100

    ssh into vm, test with simple web server

--- terraform/ephemeral/instances.tf
+++ terraform/ephemeral/instances.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = ">= 1.0.0"
- 
+
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
@@ -13,23 +13,49 @@ terraform {
 #    description = "DigitalOcean API Token"
 #    type = string
 # }
- 
+
 variable "do_token" {
-    description = "DigitalOcean API Token"
-    type = string
+  description = "DigitalOcean API Token"
+  type        = string
 }
- 
+
+# variable "pvt_key_path" {}
+
+variable "pvt_key" {}
+
 provider "digitalocean" {
-    token = var.do_token
-#   token = var.digital_ocean_token # change for the CI/CD pipeline
+  token = var.do_token
+  #   token = var.digital_ocean_token # change for the CI/CD pipeline
 }
- 
+
 resource "digitalocean_droplet" "prod" {
   image  = "ubuntu-22-04-x64"
   name   = "prod"
   region = "ams3"
   size   = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.id
+  ]
+
+  connection {
+    host        = self.ipv4_address
+    user        = "root"
+    type        = "ssh"
+    # private_key = file(var.pvt_key_path)
+    private_key = var.pvt_key
+    timeout     = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # install nginx
+      "sudo apt update",
+      "sudo apt install -y nginx"
+    ]
+  }
 }
+
 
 # output "droplet_ip_main_app" {
 #   value       = digitalocean_droplet.main-app.ipv4_address
@@ -49,4 +75,8 @@ data "terraform_remote_state" "other_workspace" {
 resource "digitalocean_reserved_ip_assignment" "example" {
   ip_address = data.terraform_remote_state.other_workspace.outputs.reserved_ip_address
   droplet_id = digitalocean_droplet.prod.id
+}
+
+data "digitalocean_ssh_key" "terraform" {
+  name = "terraform"
 }
