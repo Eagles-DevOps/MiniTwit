0fca2c83dcf66d2e53c3dc71cc09252fc6fd8def
Hopeberg <anam@itu.dk>
2024-02-29 14:02:00 +0100

    added main.tf

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_version = ">= 1.0.0"
+ 
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+variable "do_token" {
+   description = "DigitalOcean API Token"
+   type = string
+}
+ 
+provider "digitalocean" {
+  token = var.do_token
+}
+ 
+resource "digitalocean_droplet" "main-app" {
+  image  = "ubuntu-22-04-x64"
+  name   = "Main-app"
+  region = "ams3"
+  size   = "s-1vcpu-1gb"
+}
+
+output "droplet_ip_main_app" {
+  value       = digitalocean_droplet.main-app.ipv4_address
+  description = "The public IP address of the main-app droplet."
+}
+
+resource "digitalocean_droplet" "api" {
+  image  = "ubuntu-22-04-x64"
+  name   = "API-service"
+  region = "ams3"
+  size   = "s-1vcpu-1gb"
+}
+
+output "droplet_ip_api" {
+  value       = digitalocean_droplet.api.ipv4_address 
+  description = "The public IP address of the api droplet."
+}
