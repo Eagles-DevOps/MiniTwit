b8c98ca9a928a1f517a42e69459167812be51a82
Hopeberg <anam@itu.dk>
2024-03-02 20:41:42 +0100

    spin

--- provision.sh
+++ provision.sh
@@ -3,6 +3,6 @@
 export PATH=$PATH:/usr/bin
 cd /tmp/
 
-docker-compose -f /path/to/docker_compose.yml down 
-docker-compose -f /path/to/docker_compose.yml up -d 
-docker-compose -f /path/to/docker_compose.yml pull
+docker-compose -f docker_compose.yml down 
+docker-compose -f docker_compose.yml up -d 
+docker-compose -f docker_compose.yml pull
