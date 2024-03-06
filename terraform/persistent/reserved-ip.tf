e6b71e288185e06c8a21cbf304968e71fa86f1e7
Hopeberg <anam@itu.dk>
2024-03-06 14:45:59 +0100

    removed commented out code

--- terraform/persistent/reserved-ip.tf
+++ terraform/persistent/reserved-ip.tf
@@ -20,21 +20,3 @@ resource "digitalocean_reserved_ip" "prod-ip" {
 output "reserved_ip_address" {
   value = digitalocean_reserved_ip.prod-ip.ip_address
 }
-
-# data "digitalocean_account" "my_account_info" {
-# }
-
-# output "droplet_limit"{
-#   value = data.digitalocean_account.my_account_info.droplet_limit
-# }
-
-# resource "digitalocean_droplet" "web" {
-#   image  = "ubuntu-22-04-x64"
-#   name   = "web-1"
-#   region = "ams3"
-#   size   = "s-1vcpu-1gb"
-# }
-
-# output "server_ip" {
-#   value = digitalocean_droplet.web.ipv4_address
-# }
