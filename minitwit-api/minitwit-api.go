39532f995ee11c44ed9532f4af7cfe93548e413e
Hopeberg <anam@itu.dk>
2024-03-07 16:20:48 +0100

    cron job to backup database in DO spaces Co-authored by: ChatGPT

--- minitwit-api/minitwit-api.go
+++ minitwit-api/minitwit-api.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
+	"time"
 
 	"minitwit-api/api"
 
@@ -14,6 +15,9 @@ import (
 	"minitwit-api/db"
 
 	"github.com/robfig/cron/v3"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 func main() {
@@ -32,7 +36,7 @@ func main() {
 	if c == nil {
 		log.Fatal("Error creating cron instance")
 	}
-	c.AddFunc("*/15 * * * *", backup)
+	c.AddFunc("* * * * *", backup)
 	c.Start()
 	defer c.Stop()
 
@@ -46,15 +50,28 @@ func main() {
 func backup() {
 	fmt.Println("Starting backup of the database...")
 
-	if err := os.MkdirAll("./backups", 0755); err != nil {
-		fmt.Printf("Error creating destination directory: %s\n", err)
+	dbPath := os.Getenv("SQLITEPATH")
+	if dbPath == "" {
+		dbPath = "./sqlite/minitwit.db"
+	}
+
+	creds := credentials.NewStatic("DO00BXQ8HLVHRM7YL773", "dCk8uuNv7zAgZVW7mfvyIjzglZfBoGSbBoSecZskXJo", "", credentials.SignatureV4)
+
+	minioClient, err := minio.New("minitwit.ams3.digitaloceanspaces.com", &minio.Options{
+		Creds:  creds,
+		Secure: true,
+	})
+	if err != nil {
+		fmt.Printf("Error creating Minio client: %v\n", err)
 		return
 	}
-	cmd := exec.Command("scp", "-i", "~/.ssh/terraform", "-o", "StrictHostKeyChecking=no", "root@188.166.201.66:/tmp/sqlitedb-api/minitwit.db", "./backups/minitwit.db")
 
-	err := cmd.Run()
+	timestamp := time.Now().Format("2006-01-02-15-04-05")
+	fileName := fmt.Sprintf("minitwit-%s.db", timestamp)
+
+	_, err = minioClient.FPutObject(context.Background(), "minitwit", fileName, dbPath, minio.PutObjectOptions{})
 	if err != nil {
-		fmt.Printf("Error running scp command: %s\n", err)
+		fmt.Printf("Error uploading database file: %v\n", err)
 		return
 	}
 
