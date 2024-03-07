1494e38e1f79df2467b612cb15d519a7a1036605
Hopeberg <anam@itu.dk>
2024-03-07 18:15:05 +0100

    spin

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH=$PATH:/usr/bin
-cd /tmp/
-
-docker compose down 
-docker compose up -d 
-docker compose pull
--- terraform/ephemeral/provision.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export PATH=$PATH:/usr/bin
-cd /tmp/
-docker compose down
-docker compose pull
-docker compose up -d
