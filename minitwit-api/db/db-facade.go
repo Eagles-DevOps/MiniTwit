481f88abfea961e12bf8d6f9c03e4819a4a4f1fe
Roman <58208649+Romes8@users.noreply.github.com>
2024-03-13 21:03:31 +0100

    Db metrics (#189)
    Co-authored-by: Hopeberg <anam@itu.dk>

--- minitwit-api/db/db-facade.go
+++ minitwit-api/db/db-facade.go
@@ -8,12 +8,24 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
 var db *gorm.DB
+
+var (
+	readWritesDatabase = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "minitwit_database_read_writes_total",
+			Help: "Counts reads and writes to database.",
+		},
+		[]string{"func_name", "action", "status"},
+	)
+)
 
 func Connect_db() {
 	dbPath := os.Getenv("SQLITEPATH")
@@ -50,9 +62,12 @@ func Connect_db() {
 	})
 	if err != nil {
 		fmt.Println("Error connecting to the database ", err)
+		readWritesDatabase.WithLabelValues("Connect_db", "connect", "fail").Inc()
 		return
 	}
 	db.AutoMigrate(&model.User{}, &model.Follower{}, &model.Message{})
+	readWritesDatabase.WithLabelValues("Connect_db", "connect", "success").Inc()
+
 }
 
 func QueryRegister(args []string) {
@@ -61,11 +76,20 @@ func QueryRegister(args []string) {
 		Email:    args[1],
 		PwHash:   args[2],
 	}
-	db.Create(user)
+	res := db.Create(user)
+	if res.Error != nil {
+		readWritesDatabase.WithLabelValues("QueryRegister", "write", "fail").Inc()
+	}
+	readWritesDatabase.WithLabelValues("QueryRegister", "write", "success").Inc()
 }
 
 func QueryMessage(message *model.Message) {
-	db.Create(message)
+	res := db.Create(message)
+	if res.Error != nil {
+		readWritesDatabase.WithLabelValues("QueryMessage", "write", "fail").Inc()
+	}
+	readWritesDatabase.WithLabelValues("QueryMessage", "write", "success").Inc()
+
 }
 
 func QueryFollow(args []int) {
@@ -73,20 +97,35 @@ func QueryFollow(args []int) {
 		WhoID:  args[0],
 		WhomID: args[1],
 	}
-	db.Create(follower)
+	res := db.Create(follower)
+	if res.Error != nil {
+		readWritesDatabase.WithLabelValues("QueryFollow", "write", "fail").Inc()
+	}
+	readWritesDatabase.WithLabelValues("QueryFollow", "write", "success").Inc()
 }
 
 func QueryUnfollow(args []int) {
-	db.Where("who_id = ? AND whom_id = ?", args[0], args[1]).Delete(&model.Follower{})
+	res := db.Where("who_id = ? AND whom_id = ?", args[0], args[1]).Delete(&model.Follower{})
+	if res.Error != nil {
+		readWritesDatabase.WithLabelValues("QueryUnfollow", "write", "fail").Inc()
+	}
+	readWritesDatabase.WithLabelValues("QueryUnfollow", "write", "success").Inc()
 }
 
 func QueryDelete(args []int) {
-	db.Delete(&model.User{}, args[0])
+	res := db.Delete(&model.User{}, args[0])
+	if res.Error != nil {
+		readWritesDatabase.WithLabelValues("QueryDelete", "write", "fail").Inc()
+	}
+	readWritesDatabase.WithLabelValues("QueryDelete", "write", "success").Inc()
 }
 
 func GetMessages(args []int) []map[string]any {
 	var messages []model.Message
-	db.Where("flagged = 0").Order("pub_date DESC").Limit(args[0]).Find(&messages)
+	res := db.Where("flagged = 0").Order("pub_date DESC").Limit(args[0]).Find(&messages)
+	if res.Error != nil {
+		readWritesDatabase.WithLabelValues("GetMessages", "read", "fail").Inc()
+	}
 
 	var Messages []map[string]any
 	for _, msg := range messages {
@@ -100,12 +139,16 @@ func GetMessages(args []int) []map[string]any {
 
 		Messages = append(Messages, message)
 	}
+	readWritesDatabase.WithLabelValues("GetMessages", "read", "success").Inc()
 	return Messages
 }
 
 func GetMessagesForUser(args []int) []map[string]any {
 	var messages []model.Message
-	db.Where("flagged = 0 AND author_id = ?", args[0]).Order("pub_date DESC").Limit(args[1]).Find(&messages)
+	res := db.Where("flagged = 0 AND author_id = ?", args[0]).Order("pub_date DESC").Limit(args[1]).Find(&messages)
+	if res.Error != nil {
+		readWritesDatabase.WithLabelValues("GetMessagesForUser", "read", "fail").Inc()
+	}
 
 	var Messages []map[string]any
 
@@ -120,18 +163,23 @@ func GetMessagesForUser(args []int) []map[string]any {
 
 		Messages = append(Messages, message)
 	}
+	readWritesDatabase.WithLabelValues("GetMessagesForUser", "read", "success").Inc()
 	return Messages
 }
 
 func GetFollowees(args []int) []string {
 	var followees []string
-	db.Table("user").
+	res := db.Table("user").
 		Select("user.username").
 		Joins("inner join follower ON follower.whom_id=user.user_id").
 		Where("follower.who_id = ?", args[0]).
 		Limit(args[1]).
 		Scan(&followees)
 
+	if res.Error != nil {
+		readWritesDatabase.WithLabelValues("GetFollowees", "read", "fail").Inc()
+	}
+	readWritesDatabase.WithLabelValues("GetFollowees", "read", "success").Inc()
 	return followees
 }
 
@@ -140,10 +188,14 @@ func Get_user_id(username string) (int, error) {
 	res := db.Where("username = ?", username).First(&user)
 	if res.Error != nil {
 		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+			readWritesDatabase.WithLabelValues("Get_user_id", "read", "fail").Inc()
 			return 0, fmt.Errorf("user with username '%s' not found", username)
+
 		}
+		readWritesDatabase.WithLabelValues("Get_user_id", "read", "fail").Inc()
 		return 0, fmt.Errorf("error querying database: %v", res.Error)
 	}
+	readWritesDatabase.WithLabelValues("Get_user_id", "read", "success").Inc()
 	return user.UserID, nil
 }
 
