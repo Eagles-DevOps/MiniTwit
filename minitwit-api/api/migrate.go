a6c4e0aa8d355e01c8d3120e39785a273fc444dc
Daniel Grønbjerg <danielgronbjerg@gmail.com>
2024-03-14 17:01:28 +0100

    Fix spelling

--- minitwit-api/api/migrate.go
+++ minitwit-api/api/migrate.go
@@ -21,21 +21,21 @@ func Migrate(w http.ResponseWriter, r *http.Request) {
 	pgImpl.CreateUsers(&users)
 	elapsed := time.Since(start)
 
-	fmt.Println("Users migrated in ", elapsed, " ms")
+	fmt.Println("Users migrated in ", elapsed)
 
 	start = time.Now()
 	var followers = sqliteImpl.GetAllFollowers()
 	pgImpl.CreateFollowers(&followers)
 	elapsed = time.Since(start)
 
-	fmt.Println("Followers migrated in ", elapsed, " ms")
+	fmt.Println("Followers migrated in ", elapsed)
 
 	start = time.Now()
 	var messages = sqliteImpl.GetAllMessages()
 	pgImpl.CreateMessages(&messages)
 	elapsed = time.Since(start)
 
-	fmt.Println("Followers migrated in ", elapsed, " ms")
+	fmt.Println("Messages migrated in ", elapsed)
 
 	db.SetDb(pgImpl)
 
