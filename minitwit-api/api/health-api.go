ba33e84c5cd07fd9b711a1989218fb6343f17c69
Daniel Grønbjerg <danielgronbjerg@gmail.com>
2024-05-20 15:55:27 +0200

    add stress endpoint

--- minitwit-api/api/health-api.go
+++ minitwit-api/api/health-api.go
@@ -2,8 +2,16 @@ package api
 
 import (
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func Stress(w http.ResponseWriter, r *http.Request) {
+	password := []byte("stresstest")
+	bcrypt.GenerateFromPassword(password, bcrypt.MaxCost)
 	w.WriteHeader(http.StatusNoContent)
 }
