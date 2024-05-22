2bf74b6adba6b256703c2940dadbcbbdeda1edd5
Jan Lishak <janlishak@users.noreply.github.com>
2024-05-22 21:47:30 +0200

    edits

--- report/generate.sh
+++ report/generate.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 markdown_files=$(ls *.md | grep -v 'template.md' | sort -V) # Sort the files numerically and ignore template.md file
 
 
-command="pandoc -f markdown $markdown_files -s -o MSc_group_h.pdf"
+command="pandoc --columns=3 --wrap=auto $markdown_files -s -o MSc_group_h.pdf"
 
 # Infinite loop
 while true
