b7fe685cccd213f85c6192982d4db872f6b090ae
Jan Lishak <janlishak@users.noreply.github.com>
2024-05-22 20:36:46 +0200

    local generate

--- report/generate.sh
+++ report/generate.sh
@@ -2,4 +2,16 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 markdown_files=$(ls *.md | grep -v 'template.md' | sort -V) # Sort the files numerically and ignore template.md file
-pandoc $markdown_files -s -o MSc_group_h.pdf
+
+
+command="pandoc -f markdown $markdown_files -s -o MSc_group_h.pdf"
+
+# Infinite loop
+while true
+do
+    # Execute the command
+    $command
+    
+    # Optional: Add a sleep interval to avoid overwhelming the system
+    # sleep 1
+done
