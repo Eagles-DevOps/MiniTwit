dce5cb718e4f2866eae01520c36d319f909b7351
Jan Lishak <janlishak@users.noreply.github.com>
2024-05-23 13:28:23 +0200

    final changes!

--- report/generate.sh
+++ report/generate.sh
@@ -4,5 +4,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 while true
 do
     markdown_files=$(ls *.md | grep -v 'template.md' | sort -V) # Sort the files numerically and ignore template.md file
-    pandoc --columns=3 --wrap=auto $markdown_files -s -o MSc_group_h.pdf
+    pandoc -H disable_float.tex --number-sections --columns=3 --wrap=auto $markdown_files -s -o MSc_group_h.pdf
 done
+
+#-f markdown-implicit_figures
